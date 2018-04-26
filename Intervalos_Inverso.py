45d59d4df5a2cd68c703685c0a0b35f10b38d355
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-04-25 22:39:21 -0300

    Intervalos Inversos e Pares

---
+++ Intervalos_Inverso.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "@MkDVice"
+
+for i in range(100, -1, -2):    # 100 valor inicial, -1 final e 2 para marcar números pares
+    print(i)    # imprime os intervalos
