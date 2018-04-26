6133ae1057eb60dc5049d95fb24f21c7f4bbb111
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-04-25 22:12:53 -0300

    Intervalos 2.0

---
+++ Intervalos_Two.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "@MkDVice"
+
+entra_a = int(input('Digite o valor inical da lista: '))    # valor inicial dos intervalos
+entra_b = int(input('Digite o valor máximo da lista: '))    # valor final dos intervalos
+
+for i in range(entra_a, entra_b): # intervalos
+    print(i)    # imprime os intervalos
