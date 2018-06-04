34289f05f040c83eb0b56659c1e12a84453ea034
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-06-03 21:11:38 -0300

    numero_extenso
    Estudo de tuplas

---
+++ numero_por_extenso.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "@MkDVice"
+
+# Número por extenso
+
+cont = ('zero', 'um', 'dois', 'três', 'quatro',
+        'cinco', 'seis', 'sete', 'oito', 'nove',
+        'dez', 'onze', 'doze', 'treze', 'catorze',
+        'quinze', 'dezesseis', 'dezessete', 'dezoito'
+        'dezenove', 'vinte')
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente', end='')
+print(f'Você digitou o número {cont [num]}')
